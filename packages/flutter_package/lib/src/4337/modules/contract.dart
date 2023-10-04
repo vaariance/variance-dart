c84fe08837e41e3ed64b00c9a0ad9543599b16c9
maxiggle <clsyfriday@gmail.com>
2023-10-04 17:02:51 +0100

    fix: fixes all issues with contract class

--- packages/flutter_package/lib/src/4337/modules/contract.dart
+++ packages/flutter_package/lib/src/4337/modules/contract.dart
@@ -1,15 +1,17 @@
+import 'dart:developer';
 import 'dart:typed_data';
 
 import 'package:pks_4337_sdk/pks_4337_sdk.dart';
 import 'package:web3dart/crypto.dart';
 import 'package:web3dart/web3dart.dart';
 
+/// A Wrapper for the Contract Object
+/// for interacting with deployed contracts.
 class Contract {
   BaseProvider _provider;
   Contract(
     this._provider,
   );
-
 
   BaseProvider get provider => _provider;
 
@@ -17,14 +19,16 @@ class Contract {
     _provider = provider;
   }
 
-  /// The [call] function makes staticcall to a contract function
-  /// 
-  ///returns a list of decoded values.
-  Future<List<T>> call<T>(
+  /// [call] performs a [StaticCall] to a contract
+  /// @param [contractAddress] is the address of the contract
+  /// @param [abi] is the ABI of the contract
+  /// @param [methodName] is the name of the method in the contract
+  /// @param @optional [params] additional parameters for the method
+  /// @param @optional [sender] additional sender for the transaction
+  Future<List<dynamic>> call(
       EthereumAddress contractAddress, ContractAbi abi, String methodName,
       {List<dynamic>? params, EthereumAddress? sender}) {
-
-      /// gets contract function
+    /// gets contract function
     final func = getContractFunction(methodName, contractAddress, abi);
     final call = {
       'to': contractAddress.hex,
@@ -34,18 +38,23 @@ class Contract {
           : "0x",
       if (sender != null) 'from': sender.hex,
     };
-    return _provider.send<String>('eth_call', [call]).then(
-        (value) => func.decodeReturnValues(value) as List<T>);
+    log("call: ${call.toString()}");
+    return _provider.send<String>(
+        'eth_call', [call]).then((value) => func.decodeReturnValues(value));
   }
 
   ///[deployed] checks if a contract is deployed
+  /// @param [address] is the address of the contract
   Future<bool> deployed(EthereumAddress address) {
-    return _provider
+    final isDeployed = _provider
         .send<String>('eth_getCode', [address.hex])
         .then(hexToBytes)
         .then((value) => value.isNotEmpty);
+    return isDeployed;
   }
-  /// [getBalance] returns the balance of an Ethereum address
+
+  /// [getBalance] returns the balance of an address
+  /// @param [address] is the address to get the balance of
   Future<EtherAmount> getBalance(EthereumAddress address) {
     return _provider
         .send<String>('eth_getBalance', [address.hex])
@@ -53,17 +62,22 @@ class Contract {
         .then((value) => EtherAmount.fromBigInt(EtherUnit.wei, value));
   }
 
-  /// [encodeFunctionCall] calls a contract function and returns the encoded call.
+  /// [encodeFunctionCall] encodes the data for a function call to be sent to a contract
+  /// @param [methodName] is the name of the method in the contract
+  /// @param [contractAddress] is the address of the contract
+  /// @param [abi] is the ABI of the contract
+  /// returns the calldata as [Uint8List]
   static Uint8List encodeFunctionCall(String methodName,
       EthereumAddress contractAddress, ContractAbi abi, List<dynamic> params) {
     final func = getContractFunction(methodName, contractAddress, abi);
     return func.encodeCall(params);
   }
-  /// Calls a contract function from a deployed contract
-  /// 
-  /// and finds a method that matches the method name.
-  /// 
-  /// If the method name not is found, and it is more than one, it throws an exception
+
+  /// [getContractFunction] gets a contract function instance for a given method
+  /// @param [methodName] is the name of the method in the contract
+  /// @param [contractAddress] is the address of the contract
+  /// @param [abi] is the ABI of the contract
+  /// returns a [ContractFunction]
   static ContractFunction getContractFunction(
       String methodName, EthereumAddress contractAddress, ContractAbi abi) {
     final instance = DeployedContract(abi, contractAddress);
