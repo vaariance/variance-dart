8c54c625667d57a6a73c311d57ab58a49864ff83
Simon Samuel <salviensky@gmail.com>
2023-10-06 14:20:04 +0100

    chore: uploading basic address test

--- packages/flutter_package/test/common/address_test.dart
+++ packages/flutter_package/test/common/address_test.dart
@@ -7,6 +7,7 @@ import 'package:web3dart/web3dart.dart';
 import 'package:http/http.dart' as http;
 
 import 'mocks/address_test.mocks.dart';
+import 'mocks/basic_address_test.mocks.dart';
 
 final ethAddress =
     EthereumAddress.fromHex('0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5');
@@ -17,6 +18,7 @@ void main() {
     final mockEns = MockEns();
     final mockWeb3Client = MockWeb3Client();
     final mockHttpClient = MockClient();
+    final basicMockENS = BasicMockENS();
 
     test('can be created from an Ethereum address', () {
       final address = Address.fromEthAddress(ethAddress);
@@ -29,20 +31,28 @@ void main() {
     });
 
     test('can get the ENS name asynchronously', () async {
-      Future<String> getName() {
-        return Future.value("test.eth");
-      }
+      // Future<String> getName() {
+      //   return Future.value("test.eth");
+      // }
 
-      when(mockEns.withAddress(()).getName()).thenAnswer((_) => getName());
+      // when(mockEns.withAddress(()).getName()).thenAnswer((_) => getName());
+
+      // Simon: using my custom mockEns
+      when(basicMockENS
+              .withAddress('0x95222290dd7278aa3ddd389cc1e1d165cc4bafe5')
+              .getName())
+          .thenAnswer((_) => Future.value("test.eth"));
 
       Address address = Address.fromEthAddress(
           EthereumAddress.fromHex('0x95222290DD7278Aa3Ddd389Cc1E1d165CC4BAfe5'),
           ens: true);
 
+      // Simon: @Peter, how does this Address class know about the mockEns???
+      // I don't see any dependency injection here.
       expect(await address.getEnsName(), equals('test.eth'));
 
       // Verify the interaction with the mock Ens
-      verify(() => mockEns.withAddress(()).getName()).called(1);
+      // verify(() => mockEns.withAddress(()).getName()).called(1);
     });
 
     test('returns an empty string if the ENS name cannot be resolved',
