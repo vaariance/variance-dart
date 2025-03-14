1f00180b62ce99e792f7926aa8d8c1167da98ea1
Peter Anyaogu <anyaogupeter601@gmail.com>
2025-03-14 12:37:47 +0100

    feat: move Standard module interface to 7579 folder

--- lib/src/7579/modules/module.dart
+++ lib/src/7579/modules/module.dart
@@ -1,10 +1,10 @@
 import 'dart:typed_data';
 
-import 'package:variance_dart/src/interfaces/interfaces.dart';
 import 'package:variance_dart/variance_dart.dart';
 import 'package:web3_signers/web3_signers.dart';
 import 'package:web3dart/crypto.dart';
 
+import '../StandardModuleInterface/interface.dart';
 import 'executors/OwnableExecutor/ownable_executor.m.dart';
 import 'hooks/RegistryHook/registry_hook.m.dart';
 import 'validators/OwnableValidator/ownable_validator.m.dart';
@@ -12,10 +12,8 @@ import 'validators/SocialRecovery/social_recovery.m.dart';
 
 // ------------ EXECUTORS -------------- //
 part 'executors/OwnableExecutor/ownable_executor.dart';
-
 // ------------ HOOKS -------------- //
 part 'hooks/RegistryHook/registry_hook.dart';
-
 // ------------ VALIDATORS -------------- //
 part 'validators/OwnableValidator/ownable_validator.dart';
 part 'validators/SocialRecovery/social_recovery.dart';
--- lib/src/interfaces/interfaces.dart
+++ lib/src/interfaces/interfaces.dart
@@ -4,8 +4,6 @@ import 'package:eip1559/eip1559.dart';
 import 'package:web3_signers/web3_signers.dart' show PassKeyPair, Uint256;
 import 'package:web3dart/web3dart.dart';
 
-export 'StandardModuleInterface/interface.dart';
-
 import '../../variance_dart.dart'
     show
         Chain,
